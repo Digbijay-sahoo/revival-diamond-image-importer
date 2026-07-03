ca8eae96d44f14334c2e425d66c798a6fc41878b
Digbijay-sahoo <55906594+Digbijay-sahoo@users.noreply.github.com>
2026-07-03 10:09:24 +0000

    Added new builder gemstone metafields

--- app/lib/metafields.js
+++ app/lib/metafields.js
@@ -1,12 +1,50 @@
 export const PRODUCT_METAFIELDS = [
   {
+    label: "Builder Marquise Sapphire White",
+    namespace: "custom",
+    key: "builder_marquise_sapphire_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Marquise Sapphire Yellow",
+    namespace: "custom",
+    key: "builder_marquise_sapphire_yellow",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Pear Emerald White",
+    namespace: "custom",
+    key: "builder_pear_emerald_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Pear Emerald Yellow",
+    namespace: "custom",
+    key: "builder_pear_emerald_yellow",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Oval Ruby White",
+    namespace: "custom",
+    key: "builder_oval_ruby_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Oval Ruby Yellow",
+    namespace: "custom",
+    key: "builder_oval_ruby_yellow",
+    type: "file_reference",
+  },
+  {
     label: "Builder Round Sapphire White",
     namespace: "custom",
     key: "builder_round_sapphire_white",
+    type: "file_reference",
   },
   {
     label: "Builder Round Sapphire Yellow",
     namespace: "custom",
     key: "builder_round_sapphire_yellow",
+    type: "file_reference",
   },
 ];
