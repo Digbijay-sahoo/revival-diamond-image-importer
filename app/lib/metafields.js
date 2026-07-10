ee54e2c5484d7ee8593bc90f4934d0176c54c9b9
Digbijay-sahoo <55906594+Digbijay-sahoo@users.noreply.github.com>
2026-07-10 10:28:51 +0000

    Add new gemstone metafields and update ring builder mappings

--- app/lib/metafields.js
+++ app/lib/metafields.js
@@ -1,50 +1,295 @@
 export const PRODUCT_METAFIELDS = [
+  // =========================
+  // DONE (Already Created)
+  // =========================
+
   {
-    label: "Builder Marquise Sapphire White",
+  label: "Builder Marquise Sapphire White - Done",
+  namespace: "custom",
+  key: "builder_marquise_sapphire_white",
+  type: "file_reference",
+},
+{
+  label: "Builder Marquise Sapphire Yellow - Done",
+  namespace: "custom",
+  key: "builder_marquise_sapphire_yellow",
+  type: "file_reference",
+},
+{
+  label: "Builder Pear Emerald White - Done",
+  namespace: "custom",
+  key: "builder_pear_emerald_white",
+  type: "file_reference",
+},
+{
+  label: "Builder Pear Emerald Yellow - Done",
+  namespace: "custom",
+  key: "builder_pear_emerald_yellow",
+  type: "file_reference",
+},
+{
+  label: "Builder Oval Ruby White - Done",
+  namespace: "custom",
+  key: "builder_oval_ruby_white",
+  type: "file_reference",
+},
+{
+  label: "Builder Oval Ruby Yellow - Done",
+  namespace: "custom",
+  key: "builder_oval_ruby_yellow",
+  type: "file_reference",
+},
+{
+  label: "Builder Round Sapphire White - Done",
+  namespace: "custom",
+  key: "builder_round_sapphire_white",
+  type: "file_reference",
+},
+{
+  label: "Builder Round Sapphire Yellow - Done",
+  namespace: "custom",
+  key: "builder_round_sapphire_yellow",
+  type: "file_reference",
+},
+
+  // =========================
+  // Sapphire
+  // =========================
+
+  {
+    label: "Builder Oval Sapphire White",
     namespace: "custom",
-    key: "builder_marquise_sapphire_white",
+    key: "builder_oval_sapphire_white",
     type: "file_reference",
   },
   {
-    label: "Builder Marquise Sapphire Yellow",
+    label: "Builder Oval Sapphire Yellow",
     namespace: "custom",
-    key: "builder_marquise_sapphire_yellow",
+    key: "builder_oval_sapphire_yellow",
     type: "file_reference",
   },
   {
-    label: "Builder Pear Emerald White",
+    label: "Builder Pear Sapphire White",
     namespace: "custom",
-    key: "builder_pear_emerald_white",
+    key: "builder_pear_sapphire_white",
     type: "file_reference",
   },
   {
-    label: "Builder Pear Emerald Yellow",
+    label: "Builder Pear Sapphire Yellow",
     namespace: "custom",
-    key: "builder_pear_emerald_yellow",
+    key: "builder_pear_sapphire_yellow",
     type: "file_reference",
   },
   {
-    label: "Builder Oval Ruby White",
+    label: "Builder Emerald Sapphire White",
     namespace: "custom",
-    key: "builder_oval_ruby_white",
+    key: "builder_emerald_sapphire_white",
     type: "file_reference",
   },
   {
-    label: "Builder Oval Ruby Yellow",
+    label: "Builder Emerald Sapphire Yellow",
     namespace: "custom",
-    key: "builder_oval_ruby_yellow",
+    key: "builder_emerald_sapphire_yellow",
     type: "file_reference",
   },
   {
-    label: "Builder Round Sapphire White",
+    label: "Builder Cushion Sapphire White",
     namespace: "custom",
-    key: "builder_round_sapphire_white",
+    key: "builder_cushion_sapphire_white",
     type: "file_reference",
   },
   {
-    label: "Builder Round Sapphire Yellow",
+    label: "Builder Cushion Sapphire Yellow",
     namespace: "custom",
-    key: "builder_round_sapphire_yellow",
+    key: "builder_cushion_sapphire_yellow",
+    type: "file_reference",
+  },
+
+  // =========================
+  // Ruby
+  // =========================
+
+  {
+    label: "Builder Pear Ruby White",
+    namespace: "custom",
+    key: "builder_pear_ruby_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Pear Ruby Yellow",
+    namespace: "custom",
+    key: "builder_pear_ruby_yellow",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Emerald Ruby White",
+    namespace: "custom",
+    key: "builder_emerald_ruby_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Emerald Ruby Yellow",
+    namespace: "custom",
+    key: "builder_emerald_ruby_yellow",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Cushion Ruby White",
+    namespace: "custom",
+    key: "builder_cushion_ruby_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Cushion Ruby Yellow",
+    namespace: "custom",
+    key: "builder_cushion_ruby_yellow",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Heart Ruby White",
+    namespace: "custom",
+    key: "builder_heart_ruby_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Heart Ruby Yellow",
+    namespace: "custom",
+    key: "builder_heart_ruby_yellow",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Marquise Ruby White",
+    namespace: "custom",
+    key: "builder_marquise_ruby_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Marquise Ruby Yellow",
+    namespace: "custom",
+    key: "builder_marquise_ruby_yellow",
+    type: "file_reference",
+  },
+
+  // =========================
+  // Emerald
+  // =========================
+
+  {
+    label: "Builder Oval Emerald White",
+    namespace: "custom",
+    key: "builder_oval_emerald_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Oval Emerald Yellow",
+    namespace: "custom",
+    key: "builder_oval_emerald_yellow",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Emerald Emerald White",
+    namespace: "custom",
+    key: "builder_emerald_emerald_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Emerald Emerald Yellow",
+    namespace: "custom",
+    key: "builder_emerald_emerald_yellow",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Cushion Emerald White",
+    namespace: "custom",
+    key: "builder_cushion_emerald_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Cushion Emerald Yellow",
+    namespace: "custom",
+    key: "builder_cushion_emerald_yellow",
+    type: "file_reference",
+  },
+
+  // =========================
+  // Colombian Emerald
+  // =========================
+
+  {
+    label: "Builder Oval Colombian Emerald White",
+    namespace: "custom",
+    key: "builder_oval_colombian_emerald_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Oval Colombian Emerald Yellow",
+    namespace: "custom",
+    key: "builder_oval_colombian_emerald_yellow",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Pear Colombian Emerald White",
+    namespace: "custom",
+    key: "builder_pear_colombian_emerald_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Pear Colombian Emerald Yellow",
+    namespace: "custom",
+    key: "builder_pear_colombian_emerald_yellow",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Emerald Colombian Emerald White",
+    namespace: "custom",
+    key: "builder_emerald_colombian_emerald_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Emerald Colombian Emerald Yellow",
+    namespace: "custom",
+    key: "builder_emerald_colombian_emerald_yellow",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Cushion Colombian Emerald White",
+    namespace: "custom",
+    key: "builder_cushion_colombian_emerald_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Cushion Colombian Emerald Yellow",
+    namespace: "custom",
+    key: "builder_cushion_colombian_emerald_yellow",
+    type: "file_reference",
+  },
+
+  // =========================
+  // Paraiba
+  // =========================
+
+  {
+    label: "Builder Oval Paraiba White",
+    namespace: "custom",
+    key: "builder_oval_paraiba_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Oval Paraiba Yellow",
+    namespace: "custom",
+    key: "builder_oval_paraiba_yellow",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Cushion Paraiba White",
+    namespace: "custom",
+    key: "builder_cushion_paraiba_white",
+    type: "file_reference",
+  },
+  {
+    label: "Builder Cushion Paraiba Yellow",
+    namespace: "custom",
+    key: "builder_cushion_paraiba_yellow",
     type: "file_reference",
   },
 ];
